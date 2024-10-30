509e7f2bba60f5c5945a4689e5fe612cd2d613cd
limichael246 <148654427+limichael246@users.noreply.github.com>
2024-10-30 12:18:18 -0400

    Update AHBUart_dependencies.sv

--- rtl/AHBUart_dependencies.sv
+++ rtl/AHBUart_dependencies.sv
@@ -47,7 +47,7 @@ module socetlib_fifo #(
         end else begin
             // fifo <= fifo_next;
             for(int i = 0; i < DEPTH; i ++)
-                fifo[i] <= fifo_next;
+                fifo[i] <= fifo_next[i];
             write_ptr <= write_ptr_next;
             read_ptr <= read_ptr_next;
             full_internal <= full_next;
