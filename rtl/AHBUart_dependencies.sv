20cf0cf9258e64c091bf9cbadc01c07653d4ec2a
limichael246 <148654427+limichael246@users.noreply.github.com>
2024-11-03 02:29:20 -0500

    Update AHBUart_dependencies.sv

--- rtl/AHBUart_dependencies.sv
+++ rtl/AHBUart_dependencies.sv
@@ -82,14 +82,13 @@ module socetlib_fifo #(
                 overrun_next = 1'b1;
             end
 
-            // if (count == DEPTH) begin
-            //     count_next = count - (REN? 1 : 0) + (REN && WEN ? 1 : 0);
-            // end else if (count == 0) begin
-            //     count_next = count + (REN ? 1 : 0) - (REN && WEN ? 1: 0);
-            // end else begin
-            //     count_next = count + (WEN ? 1 : 0) - (REN ? 1 : 0);
-            // end
-            count_next = write_ptr_next - read_ptr_next;
+            if (count == DEPTH) begin
+                count_next = count - REN + (REN && WEN);
+            end else if (count == 0) begin
+                count_next = count + WEN - (REN && WEN);
+            end else begin
+                count_next = count + WEN - REN;
+            end
         end
     end
 
