53335061b34bb0c80939b49f5b6a51465f98c3e2
limichael246 <148654427+limichael246@users.noreply.github.com>
2024-11-06 14:06:26 -0500

    Update AHBUart_tapeout_tb.sv

--- rtl/AHBUart_tapeout_tb.sv
+++ rtl/AHBUart_tapeout_tb.sv
@@ -119,7 +119,7 @@ module uart_tb #();
 		end
 
 		//check data bits
-		int count = 0;
+		integer count = 0;
 		for(integer i = 7; i >= 0; i--) begin
 			#(pause);
 			if(tx != expected_data[i]) begin
