d7fe118400b7f773aa1014249d2f2d8ada33673a
limichael246 <148654427+limichael246@users.noreply.github.com>
2024-11-10 03:01:50 -0500

    reverting back just to check something real quick

--- rtl/AHBUart_tapeout_tb.sv
+++ rtl/AHBUart_tapeout_tb.sv
@@ -238,13 +238,15 @@ module uart_tb #();
 		#10;
 		tx_buffer_write(8'h7);
 		#10;
+		$display("Testing for all ones");
 		tx_buffer_write(8'hFF);
 		#10
 		$display("Test 1, writing to tx fifo buffer, complete!");
-	
+		
 		//Test 2: reading from the Tx_buffer
 		test_num++;
 		cts = 1'b1;
+
 		tx_external_read(8'h1, 9600);
 		tx_external_read(8'h2, 9600);
 		tx_external_read(8'h3, 9600);
